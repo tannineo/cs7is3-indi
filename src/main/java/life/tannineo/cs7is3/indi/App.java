4c69ba0e0c973d25d44af7c763fddf5bd74ee6b9
tannineo <tannineo@outlook.com>
2020-02-28 08:51:00 +0000

    init project

---
+++ src/main/java/life/tannineo/cs7is3/indi/App.java
@@ -0,0 +1,13 @@
+package life.tannineo.cs7is3.indi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
